4b6c16d4ff26fad92b0182edebf000c8758d2358
0ctapod <octaseevio@gmail.com>
2026-04-06 00:25:05 -0500

    Se agrega la funcionalidad de Agregar contenido y que se muestre en pantalla

--- app.js
+++ app.js
@@ -1,5 +1,55 @@
+function agregarTarea() {
+
+    const inputTitulo = document.getElementById("tituloTarea");
+    const selectCategoria = document.getElementById("categoriaTarea");
+    const tarea = { categoria: selectCategoria.value, titulo: inputTitulo.value, Hecho: false, Urgente: false };
+    console.log(tarea);
+    return tarea;
+}
+
+function mostrarCard(listaDeTareas) {
+    const divListaDeCards = document.getElementById("listaTareas");
+    divListaDeCards.innerHTML = "";
+    for (const task of listaDeTareas) {
+        // console.log(task);
+        divListaDeCards.innerHTML += `
+            <div class="card">
+            <div class ="card_header">
+                <div class="icono">${task.categoria}</div>
+                <div class="titulo">${task.titulo}</div>
+            </div>
+            <div class = "botones">
+                <button class="hecho">Hecho</button>
+                <button class="urgente">Urgente</button>
+                <button class="eliminar">🗑️ Eliminar</button>
+            </div>
+        </div>`
+    }
+
+}
+
 function main() {
-    console.log("Hola mundo");
+    const listaDeTareas = []
+
+
+    const botonAgregar = document.getElementById("agregarTarea");
+
+    if (botonAgregar) {
+        console.log("asdas");
+        botonAgregar.addEventListener("click", function event() {
+            const tarea = agregarTarea();
+            if (tarea) {
+                listaDeTareas.push(tarea);
+                mostrarCard(listaDeTareas);
+                console.log("asdfas", listaDeTareas);
+            }
+        });
+
+    }
+    console.log(listaDeTareas);
+    mostrarCard(listaDeTareas);
+
+
 }
 
 main();
